01450aee7ef56c71305f4326779dea1f11665e79
Su Hwan CHOI <9ou5oo@gmail.com>
2024-05-16 16:46:32 +0900

    🎉feat: initial commit

---
+++ src/main/java/org/finalpjt/hraccoon/HraccoonApplication.java
@@ -0,0 +1,13 @@
+package org.finalpjt.hraccoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HraccoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HraccoonApplication.class, args);
+	}
+
+}

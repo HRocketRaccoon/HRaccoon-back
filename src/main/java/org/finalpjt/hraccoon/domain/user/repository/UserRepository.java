56171959344a1eeee9b44f053828ca3b145b0275
Su Hwan CHOI <9ou5oo@gmail.com>
2024-05-28 15:36:37 +0900

    feat: 회원 관련 기능 구현
    - User
    - UserDetail
    - 계정 생성
    - 계정 조회 (본인)
    - 계정 정보 수정

---
+++ src/main/java/org/finalpjt/hraccoon/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.finalpjt.hraccoon.domain.user.repository;
+
+import java.util.Optional;
+
+import org.finalpjt.hraccoon.domain.user.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUserId(String userId);
+}

65a659adfe3e3b6159bdc6bc0a35de6c7c34c8ca
Su Hwan CHOI <9ou5oo@gmail.com>
2024-05-30 11:23:27 +0900

    feat: redisConfig,SecurityConfig 설정

--- src/main/java/org/finalpjt/hraccoon/config/SecurityConfig.java
+++ src/main/java/org/finalpjt/hraccoon/config/SecurityConfig.java
@@ -3,18 +3,20 @@ package org.finalpjt.hraccoon.config;
 import java.util.Arrays;
 import java.util.Collections;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.finalpjt.hraccoon.global.security.JwtAuthenticationFilter;
+import org.finalpjt.hraccoon.global.security.JwtProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -25,18 +27,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final JwtProvider jwtProvider;
+
+	private final AuthenticationEntryPoint entryPoint;
+
 	/* 허용 URL. 인증과 인가를 거치지 않을 경우 사용*/
 	private final String[] allowUrls = {
-		"/",
-		"/api/v1/create/user",
-		"/api/v1/sign-in"
+		"/api/v1/user/create",
+		"/api/v1/auth/sign-in"
 	};
 
 	@Bean
 	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용하지 않음
+			.csrf(CsrfConfigurer<HttpSecurity>::disable) // csrf 비활성화
 			.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-
 				@Override
 				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 					CorsConfiguration config = new CorsConfiguration();
@@ -50,13 +55,14 @@ public class SecurityConfig {
 					return config;
 				}
 			}))
-			.csrf(CsrfConfigurer<HttpSecurity>::disable)
+			// .exceptionHandling(handler -> handler.authenticationEntryPoint(entryPoint)) // spring security 에서 인증 실패시 예외처리
+			.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), BasicAuthenticationFilter.class)
 			.authorizeHttpRequests(requests -> requests
 				.requestMatchers(allowUrls).permitAll()
-				.requestMatchers(PathRequest.toH2Console()).permitAll() // H2 Console 접속은 모두에게 허용
+				// .requestMatchers(PathRequest.toH2Console()).permitAll() // H2 Console 접속은 모두에게 허용
 				.anyRequest().authenticated()); // 나머지 요청 인증 필요
 
-		http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // H2 Console 사용을 위한 설정
+		// http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // H2 Console 사용을 위한 설정
 		http.formLogin(AbstractHttpConfigurer::disable); // form login 비활성화
 		http.httpBasic(Customizer.withDefaults()); // http basic 인증 사용
 

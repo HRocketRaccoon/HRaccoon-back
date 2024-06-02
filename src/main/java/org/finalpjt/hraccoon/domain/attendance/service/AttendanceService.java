9a613a1580e46eaf31fcc05891ab9223d32cc128
SpecialSHipDobby <dkdi4224@naver.com>
2024-06-02 11:41:44 +0900

    chore: 근태 - 주석 제거

--- src/main/java/org/finalpjt/hraccoon/domain/attendance/service/AttendanceService.java
+++ src/main/java/org/finalpjt/hraccoon/domain/attendance/service/AttendanceService.java
@@ -27,19 +27,8 @@ public class AttendanceService {
     private final UserRepository userRepository;
     private final AttendanceRepository attendanceRepository;
 
-    // @Transactional
-    // // 받아온 userNo를 기반으로 시간,,,계산하려면 다른 것도 받아와야 하지 않나?
-    // public static User findByUserNo(Long userNo) {
-    //     // User a =attendanceRepository.findByUserNo(userNo); 
-    //     return findByUserNo(userNo);
-    // }
-
-    // public List<Attendance> getAttendanceDetailByDate(LocalDate date) {
-    //     return attendanceRepository.getAttendanceByDate(date);
-    // }
-
+    // 특정 일자의 출/퇴근 시간을 조회하는 로직
     public Attendance startend(String attendanceDate, String userNo) {
-        System.out.println("debug >>> ");
         LocalDate date = LocalDate.parse(attendanceDate);
         Attendance start = attendanceRepository.startend(date, userNo);
         System.out.println("debug result :: "+start); 
@@ -82,44 +71,29 @@ public class AttendanceService {
                 totalHours += duration.toHours();
             }
         }
-        // 1. int totalHours = attendances.stream()
-        //         //.mapToInt(a -> a.getAttendanceTotalTime().toLocalTime().getHour())
-        //         .mapToInt(a -> a.getAttendanceTotalTime().getHour())
-        //         .sum();
         System.out.println("service debug >>> totalHours : "+totalHours);
 
         double percent = workedDaysCount > 0 ? ((double) totalHours / (8 * workedDaysCount)) * 100 : 0;
-        // 2. System.out.println("service debug >>> workedDaysCount : " + workedDaysCount);
-        // double percent = (double) (totalHours / 8 * workedDaysCount) * 100;
-        // 1. double percent = (double) totalHours / 40 * 100;
-
         AttendacneWeekPercentResponseDTO response = new AttendacneWeekPercentResponseDTO();
         response.of(totalHours, percent);
 
-        // @Setter로 값 넣는 법
-        // @RequiredArgsConstructor; -> dto에 걸어주기
-        // AttendacneWeekPercentResponseDTO response = new AttendacneWeekPercentResponseDTO();
-        // response.setFormattedPercent(percent);
-        // response.setTotalWorkHours(totalHours);
-        
         return response;
     }
 
-
+    // 월의 총 근무 시간을 계산하는 로직
     public AttendacneMonthPercentResponseDTO calculateMonthlyHours(Long userNo, LocalDate date) {
-        // 월의 총 근무 시간을 계산하는 로직
+
         LocalDate startOfMonth = date.withDayOfMonth(1);
         LocalDate endOfMonth = date.withDayOfMonth(date.lengthOfMonth());
 
         List<Attendance> attendances = attendanceRepository.findByUserNoAndDateBetween(userNo, startOfMonth , endOfMonth);
         int totalHours = attendances.stream()
-                // .mapToInt(a -> a.getAttendanceTotalTime().toLocalTime().getHour())
                 .mapToInt(a -> a.getAttendanceTotalTime().getHour())
                 .sum();
 
         int workedDaysCount = calculateWorkedDays(attendances);
         double percent = workedDaysCount > 0 ? ((double) totalHours / (8 * workedDaysCount)) * 100 : 0;
-        // 1. double percent = (double) (totalHours / 8*workedDaysCount) * 100; 
+        // double percent = (double) (totalHours / 8*workedDaysCount) * 100; 
         
         AttendacneMonthPercentResponseDTO response = new AttendacneMonthPercentResponseDTO();
         response.of(totalHours, percent);
@@ -127,13 +101,7 @@ public class AttendanceService {
         return response;
     }
 
-    // public List<Attendance> getWeeklyAttendance(Long userNo) {
-    //     LocalDate today = LocalDate.now();
-    //     LocalDate startOfWeek = today.minusDays(today.getDayOfWeek().getValue() - 1);
-    //     LocalDate endOfWeek = startOfWeek.plusDays(6);
-    //     return attendanceRepository.findByUserNoAndDateBetween(userNo, startOfWeek, endOfWeek);
-    // }
-
+    // 해당 주의 요일별 근무시간 조회하는 로직
     public List<Attendance> getDailyAttendance(Long userNo) {
         LocalDate today = LocalDate.now();
         LocalDate startOfWeek = today.minusDays(today.getDayOfWeek().getValue() - 1);

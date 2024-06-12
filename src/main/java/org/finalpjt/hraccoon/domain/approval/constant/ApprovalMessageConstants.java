c0d9bf99b0d224aea9d7ff0d842c35ff66389de6
itsjaeya <higherpower0007@gmail.com>
2024-06-12 10:16:29 +0900

    fix: 결재자 이름 반환, 예외처리 메시지 수정

--- src/main/java/org/finalpjt/hraccoon/domain/approval/constant/ApprovalMessageConstants.java
+++ src/main/java/org/finalpjt/hraccoon/domain/approval/constant/ApprovalMessageConstants.java
@@ -5,6 +5,8 @@ public class ApprovalMessageConstants {
 	public static final String APPROVAL_CANCEL_SUCCESS = "결재 취소에 성공했습니다.";
 	public static final String APPROVAL_DETAIL_DATE_MISSING = "시작일과 종료일을 입력해 주세요.";
 	public static final String APPROVAL_DETAIL_CONTENT_MISSING = "내용을 입력해 주세요.";
+	public static final String APPROVAL_NOT_FOUND = "해당 결재안을 조회할 수 없습니다.";
 	public static final String APPROVAL_AUTHORITY_NOT_FOUND = "상신자에 대한 결재자를 찾을 수 없습니다.";
 	public static final String APPROVAL_REJECTION_REASON_NOT_FOUND = "반려 사유를 입력해 주세요.";
+	public static final String APPROVAL_RESPONSE_NOT_ALLOWED = "해당 결재안을 처리할 수 없습니다.";
 }

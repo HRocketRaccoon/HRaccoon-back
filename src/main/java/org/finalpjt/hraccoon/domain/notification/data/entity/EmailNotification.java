794c1ece3215191b572f50982a86e2872ce4b425
Su Hwan CHOI <9ou5oo@gmail.com>
2024-06-25 18:20:56 +0900

    refactor: EmailNotification basetimeEntity 추가

--- src/main/java/org/finalpjt/hraccoon/domain/notification/data/entity/EmailNotification.java
+++ src/main/java/org/finalpjt/hraccoon/domain/notification/data/entity/EmailNotification.java
@@ -2,6 +2,8 @@ package org.finalpjt.hraccoon.domain.notification.data.entity;
 
 import java.time.LocalDateTime;
 
+import org.finalpjt.hraccoon.global.abstracts.BaseTimeEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor
-public class EmailNotification {
+public class EmailNotification extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
